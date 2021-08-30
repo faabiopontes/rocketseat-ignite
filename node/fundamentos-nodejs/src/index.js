523a41f76ba87f2b720e54a63c9364e48c97e492
Fabio Pontes <faabiopontes@gmail.com>
2021-08-30 01:13:25 -0300

    NCI: Adding Nodemon to application

--- node/fundamentos-nodejs/src/index.js
+++ node/fundamentos-nodejs/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ message: "Hello World Ignite" });
+  res.json({ message: "Hello World Ignite3" });
 });
 
 app.listen(3333);
