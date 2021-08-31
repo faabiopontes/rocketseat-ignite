48ae4a619e6d0f80842ae7eb1e10e1e5f8ec15f5
Fabio Pontes <faabiopontes@gmail.com>
2021-08-30 22:06:04 -0300

    NCI: Using HTTP methods

--- node/fundamentos-nodejs/src/index.js
+++ node/fundamentos-nodejs/src/index.js
@@ -2,8 +2,29 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World Ignite3" });
+// GET - Read 
+app.get("/courses", (req, res) => {
+  res.json(["Course 1", "Course 2", "Course 3"]);
+});
+
+// POST - Create
+app.post("/courses", (req, res) => {
+  res.json(["Course 1", "Course 2", "Course 3", "Course 4"]);
+});
+
+// PUT - Update all
+app.put("/courses/:id", (req, res) => {
+  res.json(["Course 6", "Course 2", "Course 3", "Course 4"]);
+});
+
+// PATCH - Update specific
+app.patch("/courses/:id", (req, res) => {
+  res.json(["Course 7", "Course 2", "Course 3", "Course 4"]);
+});
+
+// DELETE - Delete
+app.delete("/courses/:id", (req, res) => {
+  res.json(["Course 2", "Course 3", "Course 4"]);
 });
 
 app.listen(3333);
